2f65d17379118992c420d98806bb62ba394fc8cb
LucasGallard <lgallard@frba.utn.edu.ar>
2026-06-18 11:32:58 -0300

    Cambio de estado de las donaciones
    Co-authored-by: Santiago Rodriguez <srodriguezgit@users.noreply.github.com>

--- Servicio-Donaciones/src/main/java/Sdonaciones/dominio/donacion/DonacionSegmentada.java
+++ Servicio-Donaciones/src/main/java/Sdonaciones/dominio/donacion/DonacionSegmentada.java
@@ -11,7 +11,30 @@ public class DonacionSegmentada {
     private CambioEstado estadoActual;
     private List<CambioEstado> estadoHistorico;
 
+    public DonacionSegmentada(Bien bien, CambioEstado estadoActual){
+        this.bien = bien;
+        this.estadoActual = estadoActual;
+    }
+
+    // modificar: getsubcategoria es propio del bien, bien.getsubcategoria...
     public Subcategoria getSubcategoria(){
         return bien.getSubcategoria();
+    }
+
+    public void cambiarEstado(CambioEstado estadoNuevo){
+        estadoHistorico.add(this.estadoActual);
+        this.estadoActual = estadoNuevo;
+    }
+
+    public Bien getBien(){
+        return this.bien;
+    }
+
+    public CambioEstado getEstado(){
+        return this.estadoActual;
+    }
+
+    public List<CambioEstado> getEstadoHistorico(){
+        return this.estadoHistorico;
     }
 }
