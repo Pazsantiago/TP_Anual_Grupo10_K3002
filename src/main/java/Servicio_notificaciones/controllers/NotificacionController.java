d5045b6eb86bccb56e199f896943d6bebf028582
GonzaoloDiez <155701922+GonzaoloDiez@users.noreply.github.com>
2026-08-27 22:06:08 -0300

    .

--- src/main/java/Servicio_notificaciones/controllers/NotificacionController.java
+++ src/main/java/Servicio_notificaciones/controllers/NotificacionController.java
@@ -3,6 +3,7 @@ package Servicio_notificaciones.controllers;
 import Servicio_notificaciones.DTOs.request.LoteNotificacionRequestDTO;
 import Servicio_notificaciones.DTOs.request.NotificacionRequestDTO;
 import Servicio_notificaciones.DTOs.response.NotificacionResponse;
+import Servicio_notificaciones.dominio.Notificacion;
 import Servicio_notificaciones.service.NotificacionService;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -19,48 +20,59 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/notificaciones")
 public class NotificacionController {
 
-  private final NotificacionService notificacionService;
+    private final NotificacionService notificacionService;
 
-  public NotificacionController(NotificacionService notificacionService) {
-    this.notificacionService = notificacionService;
-  }
+    public NotificacionController(NotificacionService notificacionService) {
+        this.notificacionService = notificacionService;
+    }
 
-  @PostMapping
-  public ResponseEntity<NotificacionResponse> enviar(
-      @Valid @RequestBody NotificacionRequestDTO request
-  ) {
-    return ResponseEntity.ok(
-        NotificacionResponse.from(notificacionService.enviar(request))
-    );
-  }
+    @PostMapping
+    public ResponseEntity<NotificacionResponse> enviar(
+            @Valid @RequestBody NotificacionRequestDTO request
+    ) {
+        return ResponseEntity.ok(
+                NotificacionResponse.from(notificacionService.enviar(request))
+        );
+    }
+    @GetMapping("tests/{pos}")
+    public ResponseEntity<NotificacionResponse> obtenerPorId(@PathVariable int pos) {
+        return ResponseEntity.ok(
+                NotificacionResponse.from(notificacionService.obtenerTodas().get(pos))
+        );
+    }
+    @PostMapping("/lotes")
+    public ResponseEntity<List<NotificacionResponse>> enviarLote(
+            @Valid @RequestBody LoteNotificacionRequestDTO request
+    ) {
+        List<NotificacionResponse> response = notificacionService.enviarLote(request)
+                .stream()
+                .map(NotificacionResponse::from)
+                .toList();
 
-  @PostMapping("/lote")
-  public ResponseEntity<List<NotificacionResponse>> enviarLote(
-      @Valid @RequestBody LoteNotificacionRequestDTO request
-  ) {
-    List<NotificacionResponse> response = notificacionService.enviarLote(request)
-        .stream()
-        .map(NotificacionResponse::from)
-        .toList();
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("/tests")
+    public ResponseEntity<List<Notificacion>> obtenerTodasTests() {
+        List<Notificacion> response = notificacionService.obtenerTodas()
+                .stream().toList();
 
-    return ResponseEntity.ok(response);
-  }
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping
+    public ResponseEntity<List<NotificacionResponse>> obtenerTodas() {
+        List<NotificacionResponse> response = notificacionService.obtenerTodas()
+                .stream()
+                .map(NotificacionResponse::from)
+                .toList();
 
-  @GetMapping
-  public ResponseEntity<List<NotificacionResponse>> obtenerTodas() {
-    List<NotificacionResponse> response = notificacionService.obtenerTodas()
-        .stream()
-        .map(NotificacionResponse::from)
-        .toList();
+        return ResponseEntity.ok(response);
+    }
 
-    return ResponseEntity.ok(response);
-  }
-
-  @GetMapping("/{id}")
-  public ResponseEntity<NotificacionResponse> obtenerPorId(@PathVariable UUID id) {
-    return ResponseEntity.ok(
-        NotificacionResponse.from(notificacionService.obtenerPorId(id))
-    );
-  }
+    @GetMapping("/{id}")
+    public ResponseEntity<NotificacionResponse> obtenerPorId(@PathVariable UUID id) {
+        return ResponseEntity.ok(
+                NotificacionResponse.from(notificacionService.obtenerPorId(id))
+        );
+    }
 
 }
